88a5b2f5d1d466de01f492d51414d3d470fff300
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-19 17:08:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,18 +11,16 @@ BoardMember.prototype.veto = function(){
 BoardMember.prototype.approve = function(){
   return 'You can do that!'
 }
-'You can do that!'
 
+BoardMember.prototype.doCharity = function(){
+  return 'I like to help people.'
+}
 
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+BoardMember.prototype.releasePressStatement = function(){
+  return 'You will see great things from Scuber.'
+}
+
+BoardMember.prototype.sayHi = function(){
+  return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+}
+
